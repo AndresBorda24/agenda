1032343f8d769627002f794563574bff2745d8c7
AndresBorda24 <anjart24@gmail.com>
2023-08-01 10:37:49 -0500

    Formulario general

--- assets/registro-vip/index.js
+++ assets/registro-vip/index.js
@@ -1,11 +1,15 @@
 import Alpine from "alpinejs";
+import img from "../partials/img";
 import form from "../partials/form";
+import selectAjax from "../partials/select-ajax";
 import "../css/registro-vip.css";
 
 window.Alpine = Alpine;
 
 document.addEventListener("alpine:init", () => {
-    Alpine.data("form", form);
+    Alpine.data("Img", img);
+    Alpine.data("Form", form);
+    Alpine.data("SelectAjax", selectAjax);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
