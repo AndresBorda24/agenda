29fb31319eabba93ec4d3573b2797698cb9120fb
AndresBorda24 <anjart24@gmail.com>
2023-08-29 11:15:10 -0500

    Ajustes chiquitos en Middleware

--- app/Middleware/HasPlanMiddleware.php
+++ app/Middleware/HasPlanMiddleware.php
@@ -3,14 +3,15 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use Slim\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class HasPlanMiddleware implements MiddlewareInterface
 {
-    public function process(Request $request, RequestHandler $handler): Response
+    public function process(Request $request, RequestHandler $handler): ResponseInterface
     {
         $user = $request->getAttribute("user");
         if ($user->hasPlan()) {
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ return function(App $app) {
         $app->group("", function(Group $app) {
             $app->redirect("/", "/agenda", 301);
             $app->get("/agenda", [IndexController::class, "agenda"])
-                ->setName("agenda")->add(HasPlanMiddleware::class);
+                ->setName("agenda")
+                ->add(HasPlanMiddleware::class);
 
             $app->post("/logout", [AuthController::class, "logout"])
                 ->setName("logout");
@@ -28,6 +29,7 @@ return function(App $app) {
             $app->group("/planes", function(Group $app) {
                 $app->get("", [IndexController::class, "planes"])
                     ->setName("planes");
+
                 $app->get("/feedback", [MpController::class, "finish"]);
             })->add(NoPlanMiddleware::class);
         })->add(AuthMiddleware::class);
@@ -35,9 +37,9 @@ return function(App $app) {
         $app->group("", function(Group $app) {
             $app->get("/registro", [IndexController::class, "registro"])
                 ->setName("registro");
+
             $app->get("/login", [IndexController::class, "login"])
                 ->setName("login");
         })->add(GuestMiddleware::class);
     })->add(SetRouteContextMiddleware::class);
-
 };
