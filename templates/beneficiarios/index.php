445b10967b48062bfc6dac44c3d787cbb12ee130
AndresBorda24 <anjart24@gmail.com>
2023-09-13 17:56:25 -0500

    Formulario de registro de beneficiario

--- templates/beneficiarios/index.php
+++ templates/beneficiarios/index.php
@@ -13,8 +13,23 @@
 
   <div class="d-flex p-1 main-container">
     <?= $this->fetch("./partials/aside.php") ?>
+    <main class="flex-grow-1 p-3">
+      <section class="mx-auto" style="max-width: 700px;">
+        <h1  class="fs-5 text-primary">Listado de beneficiarios</h1>
+        <div id="new-beneficiario-container" class="mb-3"> </div>
+
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </section>
+    </main>
   </div>
 
+  <?= $this->fetch("./beneficiarios/partials/form.php") ?>
   <?= $this->fetch("./partials/footer.php") ?>
   <?= $this->fetch("./partials/loader.php") ?>
 </body>
---
+++ templates/beneficiarios/partials/form.php
@@ -0,0 +1,159 @@
+<div
+x-data="{ state: {}, show: false }"
+x-show="show" x-cloak
+x-transition.opacity.300ms
+class="vh-100 vw-100 bg-black bg-opacity-50 fixed-top flex overflow-auto pb-5">
+  <template x-teleport="#new-beneficiario-container">
+    <button
+    type="button"
+    @click="show = true"
+    class="btn btn-warning btn-sm px-4 shadow">
+      Agregar Beneficiario!
+    </button>
+  </template>
+
+  <form
+  style="min-width: 290px; max-width: 400px;"
+  class="rounded shadow-lg m-auto form-uppercase bg-opacity-100 w-100 mt-4">
+    <header class="d-flex justify-content-between p-2 rounded-top shadow text-bg-primary">
+      <h6 class="m-0">Agregar Beneficiario</h6>
+      <button
+      @click="show = false"
+      type="button"
+      class="btn btn-close btn-close-white btn-sm"></button>
+    </header>
+
+    <section class="p-3 bg-light-subtle small">
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="tipo_doc">Tipo de Documento:</label>
+        <select
+        id="tipo_doc"
+        x-model="state.tipo_doc"
+        class="form-select form-select-sm">
+          <option hidden value="">- Selecciona -</option>
+          <option>Tarjeta de Identidad</option>
+          <option>C&eacute;dula</option>
+          <option>C&eacute;dula Extranjer&iacute;a</option>
+        </select>
+      </div>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="documento">N° de Documento:</label>
+        <input
+        id="documento"
+        autofocus
+        required
+        x-model="state.num_histo"
+        placeholder="xxxxxxxxx"
+        type="text"
+        minlength="6"
+        class="form-control form-control-sm">
+      </div>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="parentesco">Parentesco:</label>
+        <input
+        id="parentesco"
+        required
+        placeholder="Requerido"
+        x-model="state.parentesco"
+        type="text"
+        class="form-control form-control-sm">
+      </div>
+
+      <hr>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="nom1">Nombre:</label>
+        <input
+        id="nom1"
+        required
+        x-model="state.nom1"
+        placeholder="Requerido"
+        type="text"
+        class="form-control form-control-sm">
+      </div>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="nom2">Segundo Nombre:</label>
+        <input
+        id="nom2"
+        x-model="state.nom2"
+        placeholder="Opcional"
+        type="text"
+        class="form-control form-control-sm">
+      </div>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="ape1">Apellido:</label>
+        <input
+        id="ape1"
+        required
+        x-model="state.ape1"
+        placeholder="Requerido"
+        type="text"
+        class="form-control form-control-sm">
+      </div>
+
+      <div class="small mb-">
+        <label
+        class="form-label small m-0"
+        for="ape2">Segundo Apellido:</label>
+        <input
+        id="ape2"
+        x-model="state.ape2"
+        placeholder="Opcional"
+        type="text"
+        class="form-control form-control-sm">
+      </div>
+
+      <hr>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="fecha_nac">Fecha nacimiento:</label>
+        <input
+        id="fecha_nac"
+        required
+        x-model="state.fecha_nac"
+        type="date"
+        class="form-control form-control-sm">
+      </div>
+
+      <div class="small mb-2">
+        <label
+        class="form-label small m-0"
+        for="sexo">Sexo:</label>
+        <select
+        id="sexo"
+        x-model="state.sexo"
+        class="form-select form-select-sm">
+          <option hidden value="">- Selecciona -</option>
+          <option>Masculino</option>
+          <option>Femenino</option>
+        </select>
+      </div>
+    </section>
+
+    <footer class="bg-secondary rounded-bottom p-2">
+      <button
+      type="submit"
+      class="btn btn-warning btn-sm d-block  mx-auto px-5">
+        Hecho!
+      </button>
+    </footer>
+  </form>
+</div>
