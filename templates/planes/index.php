cdeeb3a8a274b14a76a9a4fc566a8afb51781a7c
AndresBorda24 <anjart24@gmail.com>
2023-10-23 09:54:11 -0500

    Ajuste vistas - Componentes

--- templates/planes/index.php
+++ templates/planes/index.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?= $this->loadAssets("planes/app") ?>
-
   <!-- Mercado Pago -->
   <script src="https://sdk.mercadopago.com/js/v2"></script>
   <title>Planes</title>
@@ -16,16 +15,30 @@
   <div class="d-flex p-1 main-container">
     <?= $this->fetch("./partials/aside.php") ?>
     <section
-    x-data="Tabs"
+    x-data="Tabs( <?= $user->hasPlanPendiente() ? 1 : 2 ?> )"
     x-bind="events"
     class="container flex-grow-1 px-md-2"
     style="min-height: 60vh;">
-      <section x-cloak x-show="tab === 1" x-transition.opacity>
+
+      <section
+      x-cloak style="max-width: 700px;"
+      x-show="tab === 1"
+      class="mx-auto"
+      x-transition.opacity>
+        <h3 class="text-center text-primary mt-3">
+          Ya Seleccionaste un plan anteriormente...
+        </h3>
+        <?= $this->fetch("./planes/partials/pendiente.php", [
+          "pref" => $pref
+        ]) ?>
+      </section>
+
+      <section x-cloak x-show="tab === 2" x-transition.opacity>
         <h2 class="text-center text-primary mt-3">Selecciona tu plan</h2>
         <?= $this->fetch("./planes/partials/plan/component.php") ?>
       </section>
 
-      <section x-cloak x-show="tab === 2" x-transition.opacity>
+      <section x-cloak x-show="tab === 3" x-transition.opacity>
         <h2 class="text-center text-primary">Selecciona un medio de pago.</h2>
         <div
         id="medios-de-pago"
--- templates/profile/partials/plan-info.php
+++ templates/profile/partials/plan-info.php
@@ -1,39 +1,37 @@
-<article class="mb-5">
-  <h2 class="fs-6">Informaci&oacute;n sobre tu Plan:</h2>
-
-  <section class="bg-secondary border border-warning rounded shadow p-3 text-light">
-    <header class="d-sm-flex justify-content-between align-items-end mb-2">
-      <span class="d-block">
-        <span class="text-warning fs-5 mb-2 m-sm-0 d-sm-block">
-          Plan: <?= $this->auth()->user()->plan("nombre") ?>
-        </span>
-        <span class="fw-light small d-sm-block">
-          $ <?= number_format(
-            (int) $this->auth()->user()->plan("valor"),
-            thousands_separator: "."
-          ) ?>
-        </span>
+<article class="bg-secondary border border-warning rounded shadow p-3 text-light">
+  <header class="d-sm-flex justify-content-between align-items-end mb-2">
+    <span class="d-block">
+      <span class="text-warning fs-5 mb-2 m-sm-0 d-sm-block">
+        Plan: <?= $this->auth()->user()->plan("nombre") ?>
       </span>
+      <span class="fw-light small d-sm-block">
+        $ <?= number_format(
+          (int) $this->auth()->user()->plan("valor"),
+          thousands_separator: "."
+        ) ?>
+      </span>
+    </span>
 
+    <?php if( $this->auth()->user()->plan("expires_at") ): ?>
       <span class="fw-bold d-block small">
         <span class="fw-light small">Expira el:</span> <br>
         <?= $this->auth()->user()->plan("expires_at") ?>
       </span>
-    </header>
+    <?php endif ?>
+  </header>
 
-    <span>Beneficios:</span>
-    <ul class="small">
-      <?php foreach(
-        explode(";", $this->auth()->user()->plan("beneficios"))
-        as $beneficio
-      ): ?>
-        <li class="small fw-light"><?= $beneficio ?>.</li>
-      <?php endforeach ?>
-    </ul>
+  <span>Beneficios:</span>
+  <ul class="small">
+    <?php foreach(
+      explode(";", $this->auth()->user()->plan("beneficios"))
+      as $beneficio
+    ): ?>
+      <li class="small fw-light"><?= $beneficio ?>.</li>
+    <?php endforeach ?>
+  </ul>
 
-    <div class="text-light fw-light">
-      <?= $this->fetch("./planes/partials/plan/exclusiones.php") ?>
-    </div>
-  </section>
+  <div class="text-light fw-light">
+    <?= $this->fetch("./planes/partials/plan/exclusiones.php") ?>
+  </div>
 </article>
 
