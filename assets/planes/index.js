c6d1d86c319afc6c0c6d074bbba30afcdb634a79
AndresBorda24 <anjart24@gmail.com>
2023-10-23 09:53:54 -0500

    Pestaña pendiente en vista Planes

--- assets/planes/index.js
+++ assets/planes/index.js
@@ -3,11 +3,13 @@ import Alpine from "alpinejs";
 import mp from "./components/mp";
 import tabs from "./components/tabs";
 import planes from "./components/planes";
+import pendiente from "./components/pendiente";
 
 document.addEventListener('alpine:init', () => {
     Alpine.data("mp", mp);
     Alpine.data("Tabs", tabs);
     Alpine.data("Planes", planes);
+    Alpine.data("PagoPendiente", pendiente);
 });
 
 window.Alpine = Alpine;
