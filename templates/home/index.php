75f17056c35fc889c2ba601d203ae0d960d7425f
Andres Borda <anjart24@gmail.com>
2023-11-08 21:11:50 -0500

    Agregar texto al home

--- templates/home/index.php
+++ templates/home/index.php
@@ -15,7 +15,7 @@
     <?= $this->fetch("./partials/aside.php") ?>
 
     <main class="flex-grow-1 mx-auto" style="max-width: 700px;">
-      <section class="mt-3">
+      <section class="mt-3 px-3">
         <p class="fw-bold text-primary mb-1">Un poco de informaci&oacute;n:</p>
         <?= $this->fetch("./partials/carrusel/index.php", [
           "img" => [
@@ -29,7 +29,47 @@
       </section>
       <hr>
 
-      <article class="mx-auto">
+      <article class="mx-auto py-3 px-4">
+        <p class="p-2 small">
+          La Clínica Asotrauma, en su compromiso ético y de desarrollo de las personas que atienden y que cuidan, ha construido una política de humanización que alineada con el modelo de atención se centra en el bienestar de los pacientes, entendiendo que en la Clínica trabajan personas que atienden personas; para dar cumplimiento a esta política desarrolla su programa de humanización que cuenta con 4 líneas de trabajo en las cuales se establecen estrategias para el logro de los objetivos trazados.
+        </p>
+
+        <p class="p-2 small">
+          Las líneas de trabajo en las que se desarrolla el programa de humanización son:
+        </p>
+
+        <ul class="small">
+          <li>
+            <span class="fw-bold">Línea de cultura de la humanización:</span>
+            <p>
+              Se trata de extender la cultura de la humanización en el conjunto del sistema. Para ello se propone sensibilizar al conjunto de profesionales utilizando la formación como estrategia de sensibilización y la participación como mecanismo de implicación.
+            </p>
+          </li>
+          <li>
+            <span class="fw-bold">Línea de calidez del trato:</span>
+            <p>
+              Responde a la necesidad de dar el mismo trato que nos gustaría recibir, teniendo presente la vulnerabilidad de las personas en sus contactos con el servicio de salud.
+            </p>
+          </li>
+          <li>
+            <span class="fw-bold">Línea de información y comunicación:</span>
+            <p>
+              La mejora de información y comunicación son pasos necesarios para facilitar el empoderamiento para la salud, entendido este como el proceso mediante el que las personas adquieren un mayor control sobre las decisiones y acciones que afectan a su salud.
+            </p>
+          </li>
+          <li>
+            <span class="fw-bold">Línea de adecuación del entorno:</span>
+            <p>
+              El desarrollo de una verdadera cultura de la humanización requiere que se den las condiciones en las que esta pueda desarrollarse, por lo que es necesario construir entornos facilitadores.
+            </p>
+          </li>
+        </ul>
+
+
+        <p class="p-2 small">
+          Entre estos entornos se incluyen los cambios en la infraestructura, los cambios organizativos, los servicios que es preciso contratar y la mejora del ambiente laboral.
+        </p>
+
         <h4 class="text-secondary">Responsabilidad Social Empresarial</h4>
         <p class="p-2 small">
           Para la Clínica Asotrauma
