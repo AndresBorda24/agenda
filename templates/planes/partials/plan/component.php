483332c8194d23c6cd6f6bab8b5838912cbb415f
AndresBorda24 <anjart24@gmail.com>
2023-11-30 16:35:29 -0500

    Remove temporal de compra online

--- templates/planes/partials/plan/component.php
+++ templates/planes/partials/plan/component.php
@@ -8,7 +8,7 @@
 <form
 x-data="Planes"
 x-transition
-@submit.prevent="confirmPlan">
+@submit.prevent="() => false && confirmPlan()">
   <div class="planes-container row-cols-12 row-cols-md-4  p-4">
     <?php foreach($planes as $plan): ?>
       <?= $this->fetch("./planes/partials/plan/plan.php", [
@@ -17,17 +17,29 @@ x-transition
     <?php endforeach ?>
   </div>
   <hr>
-  <?= $this->fetch("./planes/partials/checks.php") ?>
 
-  <div class="mt-3">
-    <div
-      style="max-width: 280px;"
-      class="info-plan bg-dark rounded small mx-auto shadow mb-2"
-    ></div>
-    <button
-    type="submit"
-    class="planes-next-btn">
-      Continuar
-    </button>
-  </div>
+  <?php if(false): ?>
+    <?= $this->fetch("./planes/partials/checks.php") ?>
+
+    <div class="mt-3">
+      <div
+        style="max-width: 280px;"
+        class="info-plan bg-dark rounded small mx-auto shadow mb-2"
+      ></div>
+      <button
+      type="submit"
+      class="planes-next-btn">
+        Continuar
+      </button>
+    </div>
+  <?php else: ?>
+    <section class="mx-auto d-flex align-items-center mb-4 p-2 small border-start border-5 border-danger rounded-end shadow" style="background-color: #ffdede; max-width: 700px;">
+      <?= $this->fetch("./icons/sign.php", [
+        "props" => 'style="min-width: 55px; height:60px;"'
+      ]) ?>
+      <span class="fs-6 px-4">
+        Para realizar la compra de tu plan, por favor dirígete <span class="fw-bold">directamente a la Clínica</span>. Lamentamos las molestias.
+      </span>
+    </section>
+  <?php endif ?>
 </form>
