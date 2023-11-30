5e17cbe0b21dd1c6a025fb129a9d947139445a96
AndresBorda24 <anjart24@gmail.com>
2023-11-30 16:15:22 -0500

    Config

--- app/Utilities.php
+++ app/Utilities.php
@@ -36,3 +36,28 @@ if (! function_exists('App\uppercase')) {
         );
     }
 }
+
+/*
+ * dd() with headers
+ */
+if (!function_exists('ddh')) {
+    function ddh($var){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+        dump($var);
+        die;
+    }
+}
+
+/*
+ * dump() with headers
+ */
+if (!function_exists('dumph')) {
+    function dumph($var){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+        dump($var);
+    }
+}
