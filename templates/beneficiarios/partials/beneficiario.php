0913047754d73399bb5078da3f0a1578d8a17a18
AndresBorda24 <anjart24@gmail.com>
2024-03-13 14:04:23 -0500

    Change posision boton editar beneficiario

--- templates/beneficiarios/partials/beneficiario.php
+++ templates/beneficiarios/partials/beneficiario.php
@@ -5,14 +5,14 @@
   <div class="small flex-grow-1 ps-2">
     <p class="m-0">
       <span x-text="nombre" class="text-uppercase"></span>
-      <button 
-        @click="edit"
-        class="d-inline-block btn btn-sm btn-warning beneficiario-edit-btn ms-2 py-0 px-1 shadow"
-      >
-        <?= $this->fetch("./icons/edit.php") ?>
-      </button>
     </p>
     <span x-text="data.documento" class="badge text-bg-light border"></span>
   </div>
   <span class="badge text-bg-primary shadow" x-text="data.parentesco"></span>
+  <button 
+    @click="edit"
+    class="d-inline-block btn btn-sm btn-warning beneficiario-edit-btn mx-2 py-0 px-1 shadow"
+  >
+    <?= $this->fetch("./icons/edit.php") ?>
+  </button>
 </li>
