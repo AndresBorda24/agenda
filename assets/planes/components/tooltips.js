00cf081cfef031533de03e277293b9977b75acc3
AndresBorda24 <anjart24@gmail.com>
2024-04-08 15:11:17 -0500

    Add Tooltip para beneficios

--- assets/planes/components/tooltips.js
+++ assets/planes/components/tooltips.js
@@ -1,4 +1,5 @@
 import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
 import tippy from 'tippy.js';
 
 tippy.setDefaultProps({allowHTML: true, trigger: 'mouseenter click'})
@@ -6,5 +7,15 @@ tippy.setDefaultProps({allowHTML: true, trigger: 'mouseenter click'})
 document.addEventListener("DOMContentLoaded", () => {
     tippy(".exclusiones-tooltip", {
         content: document.getElementById("exclusiones-tmp")?.innerHTML
-    })
+    });
+
+    document.querySelectorAll(".beneficios-tooltip").forEach(el => {
+        const _id = el.getAttribute("data-plan");
+        if (! _id) return;
+
+        tippy(el, {
+            content: document.querySelector(`#lista-beneficios-${_id}`)?.innerHTML,
+            theme: "light"
+        });
+    });
 })
--- templates/planes/partials/plan/beneficios.php
+++ templates/planes/partials/plan/beneficios.php
@@ -1,5 +1,5 @@
 <?php $b = array_map(fn($x) => trim($x), explode(";", $beneficios)) ?>
-<ul class="d-flex flex-column my-4 small flex-grow-1 p-0">
+<ul class="d-none d-lg-flex flex-column my-4 small flex-grow-1 p-0" id="lista-beneficios-<?= $planId ?>">
   <?php foreach ($b as $beneficio): ?>
     <li class="px-3 py-1 d-flex gap-1 small">
       <span class="text-primary">
@@ -11,3 +11,11 @@
     </li>
   <?php endforeach ?>
 </ul>
+
+<div class="d-block d-lg-none p-2 text-center">
+  <span class="fs-6">Beneficios </span>
+  <span
+    data-plan="<?=$planId?>"
+    class="beneficios-tooltip text-bg-dark d-inline-block ms-2 small px-2 rounded-3 user-select-none"
+  >?</span>
+</div>
