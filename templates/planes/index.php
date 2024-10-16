efb7faf6f6a7ef32e9fd6d412bb7d4e66b0f7e8f
Andrés Borda <anjart24@gmail.com>
2024-10-15 22:03:47 -0500

    Planes vite

--- templates/planes/index.php
+++ templates/planes/index.php
@@ -3,12 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?= $this->loadAssets("planes/app") ?>
+  <?php if ($_ENV["APP_ENV"] !== "dev") {
+      echo $this->loadAssetsVite("src/main.js");
+  } ?>
   <title>Planes</title>
 </head>
 <body>
   <?= $this->fetch("./partials/header.php", [
-    "title" => "Planes"
+      "title" => "Planes",
   ]) ?>
   <div class="d-flex p-1 main-container">
     <?= $this->fetch("./partials/aside.php") ?>
@@ -18,7 +20,9 @@
       class="container flex-grow-1 px-md-3 overflow-auto"
       style="min-height: 60vh;"
     >
-      <p class="text-center px-2 py-3 rounded bg-primary-subtle small mt-4 shadow-sm"> Al realizar una compra, usted reconoce y acepta nuestros <?=  $this->fetch('./partials/tyc.php') ?> </p>
+      <p class="text-center px-2 py-3 rounded bg-primary-subtle small mt-4 shadow-sm"> Al realizar una compra, usted reconoce y acepta nuestros <?= $this->fetch(
+          "./partials/tyc.php"
+      ) ?> </p>
 
       <section
         x-cloak style="max-width: 700px;"
@@ -35,7 +39,7 @@
       <section x-cloak x-show="tab === 2" x-transition.opacity @show-gateways="() => tab = 3;">
         <h2 class="text-center text-primary mt-3 fw-bold">Selecciona tu plan</h2>
         <?= $this->fetch("./planes/partials/plan/component.php", [
-          "planes" => $planes
+            "planes" => $planes,
         ]) ?>
       </section>
 
@@ -44,8 +48,12 @@
         <button class="btn btn-sm text-muted" type="button" @click="tab = 2;">< Volver</button>
         <div class="mx-auto" style="max-width: 800px;">
           <div class="d-grid" style="font-size: 13px;">
-            <?= $this->fetch("./planes/partials/medios-pago/gou-micrositio-api.php") ?>
-            <?= $this->fetch("./planes/partials/medios-pago/gou-micrositio.php") ?>
+            <?= $this->fetch(
+                "./planes/partials/medios-pago/gou-micrositio-api.php"
+            ) ?>
+            <?= $this->fetch(
+                "./planes/partials/medios-pago/gou-micrositio.php"
+            ) ?>
           </div>
         </div>
       </section>
@@ -61,6 +69,10 @@
     </ul>
   </template>
 
+  <?php if ($_ENV["APP_ENV"] == "dev"): ?>
+    <script type="module" src="http://localhost:5173/@vite/client"></script>
+    <script type="module" src="http://localhost:5173/assets/planes/index.js"></script>
+  <?php endif; ?>
   <?= $this->fetch("./partials/footer.php") ?>
   <?= $this->fetch("./partials/loader.php") ?>
 </body>
