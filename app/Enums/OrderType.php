77aae8d9122cecc4839ca62afdf435ddc8872101
AndresBorda24 <anjart24@gmail.com>
2024-10-23 15:52:12 -0500

    Cambio de nombre a tipo de orden

--- app/Enums/OrderType.php
+++ app/Enums/OrderType.php
@@ -9,7 +9,7 @@ use App\GatewaysResponseHandlers\FidelizacionHandler;
 enum OrderType: int
 {
     case FIDELIZACION = 1;
-    case CERT_NO_ATENCION = 2;
+    case CRT_ATENCION = 2;
 
     /**
      * @return string Nombre de la clase encargada de manejar las respuestas de
@@ -19,7 +19,7 @@ enum OrderType: int
     {
         return match ($this) {
             self::FIDELIZACION => FidelizacionHandler::class,
-            self::CERT_NO_ATENCION => FidelizacionHandler::class
+            self::CRT_ATENCION => FidelizacionHandler::class
         };
     }
 }
