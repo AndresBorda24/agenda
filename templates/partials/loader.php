4bc7afa05b595bd66f4a847c2dd20bb78ca8b3a6
AndresBorda24 <anjart24@gmail.com>
2024-10-29 08:24:32 -0500

    Remove !important bootstrap

--- templates/partials/loader.php
+++ templates/partials/loader.php
@@ -1,8 +1,10 @@
 <div id="loader"
-class="vh-100 vw-100 d-none fixed-top bg-black bg-opacity-75 flex"
+class="h-screen w-screen hidden fixed-top bg-black/60"
 style="z-index: 3000;">
-    <div class="m-auto text-bg-dark bg-opacity-100 rounded p-2 flex flex-column justify-center items-center">
-        <img src="<?= $this->asset("/img/logo-color.png") ?>" alt="loader" width="50">
-        <span class="text-light d-block">Cargando...</span>
+    <div class="grid place-content-center h-full w-full">
+        <div class="m-auto text-bg-dark bg-opacity-100 rounded p-2 flex flex-column justify-center items-center">
+            <img src="<?= $this->asset("/img/logo-color.png") ?>" alt="loader" width="50">
+            <span class="text-light d-block">Cargando...</span>
+        </div>
     </div>
 </div>
