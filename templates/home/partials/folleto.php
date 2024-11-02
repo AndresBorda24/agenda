ab421d5248954bc5948f4df7d168931b3af0fce2
AndresBorda24 <anjart24@gmail.com>
2024-11-02 08:47:58 -0500

    Modificación de Video Para Solicitud de Citas en Landing

--- templates/home/partials/folleto.php
+++ templates/home/partials/folleto.php
@@ -1,11 +1,11 @@
 <div
-  class="mx-auto text-bg-dark mb-4 p-4 text-center rounded shadow-lg"
+  class="mx-auto text-bg-dark mb-4 p-8 text-center rounded shadow-lg"
   style="max-width: 400px;"
 >
   <span class="text-warning block w-min mx-auto" style="font-size: 3rem;">
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M15 14h3V9h-5V4H6v16h9z" opacity="0.3"/><path fill="currentColor" d="M15 22H6c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h8l6 6v6h-2V9h-5V4H6v16h9zm4-.34v-2.24l2.95 2.95l1.41-1.41L20.41 18h2.24v-2H17v5.66z"/></svg>
   </span>
-  <p class="small"><span class="text-warning fs-5">¿Tienes dudas?</span> <br> <br> Puedes ver el Folleto del <i class="fw-bold">Programa de Fidelización</i> dando click en el siguiente botón:</p>
+  <p class="small"><span class="text-warning fs-5">¿Tienes dudas?</span> <br> <br> Puedes ver el Folleto del <i class="fw-bold text-nowrap">Programa de Fidelización</i> dando click en el siguiente botón:</p>
   <a
     href="<?= $this->asset('Folleto programa de fidelización.pdf') ?>"
     class="d-block mx-auto btn btn-warning shadow-lg px-5"
